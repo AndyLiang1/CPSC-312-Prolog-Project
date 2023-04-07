2b9b9d43fbdd400abca0d8bfe63f85aa45a54da7
AndyLiang1 <andyliang1000@gmail.com>
2023-04-07 16:23:57 -0700

    Can now add the location dynamically to our knowledge base. But currently still only hard coded to dialga...

--- project2.pl
+++ project2.pl
@@ -3,19 +3,23 @@
 
 :- use_module(library(http/http_client)).
 :- use_module(library(http/json)).
+:- dynamic location/2.
+
 
 query_api(_, Result) :- 
     http_get("https://pokeapi.co/api/v2/pokemon/dialga/encounters", Response, []),
     atom_json_dict(Response, Data, []),
-    write(Data),
-    getAllLocations(Data, Result).
-
-getAll([], []).
-getAll([Object | T], [Object.userId | ResultsSoFar]) :-
-    getAll(T, ResultsSoFar).
-
+    getAllLocations(Data, AllLocations),
+    insertLocationIntoKnowledgeBase(AllLocations),
+    Result = AllLocations.
 
 getAllLocations([], []).
 getAllLocations([OneLocation | T], [OneLocation.location_area.name | ResultsSoFar]) :-
     getAllLocations(T, ResultsSoFar).
+
+insertLocationIntoKnowledgeBase([]).
+insertLocationIntoKnowledgeBase([LocationAsString | T]) :- 
+    assertz((location(dialga, LocationAsString))),
+    insertLocationIntoKnowledgeBase(T).
+
 
