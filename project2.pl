2a1decb09fa698eef6d8bd42be77f7b9df750c3f
AndyLiang1 <andyliang1000@gmail.com>
2023-04-07 14:30:38 -0700

    Can now extract a pokemon's location.

--- project2.pl
+++ project2.pl
@@ -4,15 +4,18 @@
 :- use_module(library(http/http_client)).
 :- use_module(library(http/json)).
 
-query_api(URL, Result) :- 
-    http_get(URL, Response, []),
+query_api(_, Result) :- 
+    http_get("https://pokeapi.co/api/v2/pokemon/dialga/encounters", Response, []),
     atom_json_dict(Response, Data, []),
     Result = Data.
 
 
+getAllLocations([], []).
+getAllLocations([{"location_area": {"name": V, _}, _} | T], [V | ResultsSoFar]) :- 
+    getAllLocations(T, ResultsSoFar).
 
 
 
-
-
-
+extractOneProp([], []).
+extractOneProp([{"a": V,_} | T], [V | ResultSoFar]) :- 
+    extractOneProp(T, ResultSoFar).
