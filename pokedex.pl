14d70e6afb059b0b24e81f824e6d091c61fcb9c0
lluey <lluey@students.ubc.ca>
2023-04-10 19:04:00 -0700

    Added encounters

--- pokedex.pl
+++ pokedex.pl
@@ -54,19 +54,48 @@ q(Ans) :-
     read_line_to_string(user_input, St),
     split_string(St, " ", " ,?.!-", Ln), % ignore punctuation
     question(Ln, Ans).
+/*
 q(Ans) :-
     write("No more answers or invalid query\n"),
     write("Would you like to exit? "), flush_output(current_output),
     read_line_to_string(user_input, St0),
     string_lower(St0, St),
     q(St, Ans).
+*/
 
-q("yes", _).
-q("no", Ans) :- q(Ans).
+start(Ans) :-
+    write("\nList of commands:\n"),
+    write("e - Enter a new Pokemon encounter.\n"),
+    write("q - Ask a question.\n"),
+    write("l - List all encountered or searched Pokemon.\n"),
+    write("c - Clear history.\n"),
+    write("Input: "),
+    read_line_to_string(user_input, St),
+    handle_command(St, Ans).
+
+start(Ans) :-
+    write("No more answers or invalid query\n"),
+    write("Would you like to exit? "), flush_output(current_output),
+    read_line_to_string(user_input, St0),
+    string_lower(St0, St),
+    end(St, Ans).
+
+handle_command("e", Ans) :- 
+    write("Enter encounter: "),
+    read_line_to_string(user_input, St),
+    addEncounter(St, Ans).
+
+handle_command("q", Ans) :- q(Ans).
+handle_command("l", Ans) :- getEncounter(Ans).
+handle_command("c", Ans) :- clearKB(Ans).
+
+end("yes", _).
+end("no", Ans) :- start(Ans).
 
 
 % location("dialga", Location)
-% q(Ans).
+% start(Ans).
+% q.
 % What type is Dialga?
 % What Pokemon is in spear-pillar-area?
-% What is at 
+
